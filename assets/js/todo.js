964208db659511eaf86a47db801ed496ba958bca
Vinicius Ferreira <viniciusant3@gmail.com>
2020-04-29 11:40:43 -0300

    finished todo app

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,1 +1,26 @@
-alert("connected")
+$("ul").on("click", "li", function(){
+    // $(this).css({
+    //     "color":"grey",
+    //     "text-decoration":"line-through"
+    // })
+    $(this).toggleClass("done");
+});
+
+$("ul").on("click", "li span", function (e) { 
+    e.preventDefault();
+    $(this).parent().fadeOut(300, function(){
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
+
+$("input").on("keypress", function (e) {
+    if(e.which === 13 && $(this).val()!==""){
+        $("ul").append(`<li><span><i class= "fas fa-trash"></i></span> ${$(this).val()}</li>`);
+        $(this).val("");
+    };
+});
+
+$(".fa-list").on("click", function () {
+    $("input").fadeToggle();
+});
